a58f1c77460745462a563a79289498159b225d16
apple <apple@apples-MacBook-Air.local>
2022-04-21 14:58:44 +0100

    init commit

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
 const functions = require("firebase-functions");
+const express = require("express");
+
+//
+const app = express();
+//
+app.get("/hi", (req, res) => {
+    res.end("hi man!!");
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((app));
