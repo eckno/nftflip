7b1d93c5e554d20b9a93b984f692e30aee1377aa
apple <apple@apples-MacBook-Air.local>
2022-05-08 16:06:44 +0100

    on dashboard

--- functions/routes/index_route.js
+++ functions/routes/index_route.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const {LANDING_PAGE, LOGIN, REGISTERATION, EMAIL} = require("../lib/index-routes");
+const {DASHBOARD} = require("../lib/user-routes");
 const {LANDING_PAGE, LOGIN, REGISTERATION, VERIFICATION} = require("../lib/index-routes");
 const indexController = require("../controller/index");
 const controller = new indexController();
 const verifyUser = require("../middleware/verification");
+const passport = require("passport");
 
 
 router.get(LANDING_PAGE, async (req, res) => {
-    res.render("index", {title: 'home page'});
+    res.render("index", {title: 'Home Page'});
 });
 
 router.get(LOGIN, async (req, res) => {
-    return controller.loginController(req, res);
+   return controller.loginController(req, res);
 });
 
 router.get(REGISTERATION, async (req, res) => {
@@ -28,8 +29,28 @@ router.post(REGISTERATION, async (req, res) => {
     return controller.registerController(req, res);
 });
 
-router.post(LOGIN, async (req, res) => {
-    return controller.loginController(req, res);
+router.post(LOGIN, async (req, res, next) => {
+    return controller.loginController(req, res, next);
+    // passport.authenticate("local", (error, user, info) => {
+    //     if(error) {
+    //         const data = {success: false, msg: error}
+    //         return res.status(500).json(data);
+    //     }
+    //     if(!user) {
+    //         const data = {success: false, msg: info.message}
+    //         return res.status(401).json(data);
+    //     }
+    //     console.log("UserItem ", user);
+    //     req.logIn(user, function(err) {
+    //         if (err) { return next(err); }
+    //         const data = {success: true, data: user, redirectURL: "/dashboard"}
+    //         return res.json(data);
+    //         //return res.redirect('/users/' + user.username);
+    //       });
+        
+    // })(req, res, next);
+    //
+
 });
 
 router.post(VERIFICATION, async (req, res) => {
